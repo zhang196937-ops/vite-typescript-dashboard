020f69434d4c6aaf8b90fb2f11faa1084d8657dc
zhang196937-ops <zhang196937@gmail.com>
2026-02-23 18:23:14 -0500

    create files

---
+++ titanic_backend/analytics/load_data.py
@@ -0,0 +1,30 @@
+import csv
+import os
+from .models import Passenger
+
+def load_titanic_data():
+    # path to csv file.
+    csv_path = os.path.join('data','titanic.csv')
+    #clear existing data
+    Passenger.objects.all().delete()
+    #read csv and create passengers
+    with open(csv_path,'r',encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+
+        for row in reader:
+            Passenger.objects.create(
+                survived=bool(int(row['Survived'])),
+                pclass=int(row['Pclass']),
+                name=row['Name']
+                sex=row['Sex']
+                age=float(row['Age']) if row['Age'] else None,
+                sibsp=int(row['SibSp']),
+                parch=int(row['Parch']),
+                fare=float(row['fare']) if row['fare'] else None,
+                embarked=row['Embarked'] if row['Embarked'] else None
+            )
+    print(f"Loaded {Passenger.objects.count()} passengers!")
+
+if __name__=="__main__":
+    load_titanic_data()
+              
--- titanic_backend/analytics/models.py
+++ titanic_backend/analytics/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Passenger(models.Model):
+    passage_id = models.AutoField(primary_key=True)
+    survived=models.BooleanField()
+    pclass = models.IntegerField()
+    name=models.CharField(max_length=100)
+    sex=models.CharField(max_length=10)
+    age=models.FloatField(null=True,blank= True)
+    sibsp=models.IntegerField(default=0)
+    parch=models.IntegerField(default=0)
+    fare=models.FloatField(null=True,blank=True)
+    embarked=models.CharField(max_length=1,null=True,blank=True)
+    # class Meta = settings for this model
+    #Just tells Django: "Name the database table passengers, not analytics_passenger"
+    class Meta:
+        db_table="passengers"
+    def __str__(self):
+        return self.name
+                              
